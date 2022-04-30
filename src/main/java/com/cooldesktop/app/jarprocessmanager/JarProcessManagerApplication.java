db558adadac599dfa326629e7b777e684bf6e178
2606710413@qq.com <2606710413@qq.com>
2022-05-01 01:54:13 +0800

    init

---
+++ src/main/java/com/cooldesktop/app/jarprocessmanager/JarProcessManagerApplication.java
@@ -0,0 +1,13 @@
+package com.cooldesktop.app.jarprocessmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarProcessManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JarProcessManagerApplication.class, args);
+    }
+
+}
